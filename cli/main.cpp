8f75360109a6a2699a7ddfb4e7728e31082638bf
Marcos Mayorga <marcos.mayorga@nem.software>
2021-05-30 14:49:31 +0100

    build!: Initial files
    Hello World console program providing a template for new project baselines.
    
    Includes directories for shared library sources (src), console executable (cli), and unit test.

---
+++ cli/main.cpp
@@ -0,0 +1,22 @@
+#include <myproject/Hmi.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char** argv) {
+	using namespace std;
+	using Cli = my_offline_project::Hmi;
+
+    ostringstream os;
+    for (int i=1; i< argc; ++i) {
+        os << argv[i] << ' ';
+    }
+    string cmdline=os.str();
+
+    Cli::Section::init(cout, cerr, "0.0.1");
+    Cli cli;
+    cli.init(argv[0], "My Project - offline.");
+	//TODO: interactive shell
+    return cli.exec(cmdline)?0:1;
+}
+
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <symbol/core.h>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+using namespace symbol;
+
+int main() {
+	return 0;
+}
+
