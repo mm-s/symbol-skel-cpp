f8698acded418a4045047a932c89122251cb371e
Marcos Mayorga <marcos.mayorga@nem.software>
2021-06-04 13:14:08 +0100

    build: Automated syncing files published elsewhere
    cmakemodules Find*.cmake

--- cmake/Modules/FindSYMBOLCORE.cmake
+++ cmake/Modules/FindSYMBOLCORE.cmake
@@ -1,38 +1,41 @@
-# - Find Symbolcore - https://github.com/nemtech/symbol-sdk-core-cpp
-# Find the native Symbolcore headers and libraries.
+# marc/os - NEM Software
+#Find the native Symbolcore headers and libraries, sourced at https://github.com/nemtech/symbol-sdk-core-cpp
 #
-#  SYMBOLCORE_INCLUDE_DIRS   - where to find
-#  SYMBOLCORE_LIBRARIES      - List of libraries when using it.
-#  SYMBOLCORE_FOUND          - True if found.
+#Usage:
+#	FindPackage(SYMBOLCORE)
+#
+#Defines:
+#	SYMBOLCORE_INCLUDE_DIRS   - where to find
+#	SYMBOLCORE_LIBRARIES      - List of libraries when using it.
+#	SYMBOLCORE_FOUND          - True if found.
 
 find_path(SYMBOLCORE_INCLUDE_DIR symbol/core/core.h
-  /usr/include
-  /usr/local/include
-  DOC "Directory containing symbolcore headers"
+	/usr/include
+	/usr/local/include
+	DOC	"Directory containing symbolcore headers"
 )
 mark_as_advanced(SYMBOLCORE_INCLUDE_DIR)
 
-FIND_LIBRARY( SYMBOLCORE_LIBRARY
-  NAMES symbolcore
-  PATHS /usr/lib
-        /usr/local/lib
-	/usr/lib/i386-linux-gnu/
-	/usr/lib/x86_64-linux-gnu/
+find_library( SYMBOLCORE_LIBRARY
+	NAMES	symbolcore
+	PATHS	/usr/lib
+		/usr/local/lib
+		/usr/lib/i386-linux-gnu/
+		/usr/lib/x86_64-linux-gnu/
 )
-
 mark_as_advanced(SYMBOLCORE_LIBRARY)
 
-  if ( NOT SYMBOLCORE_INCLUDE_DIR OR NOT SYMBOLCORE_LIBRARY )
-    if ( SYMBOLCORE_REQUIRED )
-      message( FATAL_ERROR "symbolcore is required." )
-    endif ()
-  else ()
-    set( SYMBOLCORE_FOUND 1 )
-    mark_as_advanced( SYMBOLCORE_FOUND )
-  endif ()
-
+if ( NOT SYMBOLCORE_INCLUDE_DIR OR NOT SYMBOLCORE_LIBRARY )
+	if ( SYMBOLCORE_REQUIRED )
+		message( FATAL_ERROR "symbolcore is required." )
+	endif ()
+else ()
+	set( SYMBOLCORE_FOUND 1 )
+	mark_as_advanced( SYMBOLCORE_FOUND )
+endif ()
 
 if(SYMBOLCORE_FOUND)
-  set(SYMBOLCORE_LIBRARIES ${SYMBOLCORE_LIBRARY})
-  set(SYMBOLCORE_INCLUDE_DIRS ${SYMBOLCORE_INCLUDE_DIR})
+	set(SYMBOLCORE_LIBRARIES ${SYMBOLCORE_LIBRARY})
+	set(SYMBOLCORE_INCLUDE_DIRS ${SYMBOLCORE_INCLUDE_DIR})
 endif()
+
