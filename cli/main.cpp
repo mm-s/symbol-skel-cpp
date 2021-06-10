57a2ca0ccf81fdb065ed4fcb69eef8f4f9d238ad
Marcos Mayorga <marcos.mayorga@nem.software>
2021-06-10 19:55:51 +0100

    style: tabs spaces

--- cli/main.cpp
+++ cli/main.cpp
@@ -7,32 +7,32 @@ int main_offline(int argc, char** argv) {
 	using namespace std;
 	using Cli = my_namespace::MyHmiOffline;
 
-    ostringstream os;
-    for (int i=1; i< argc; ++i) {
-        os << argv[i] << ' ';
-    }
-    string cmdline=os.str();
+	ostringstream os;
+	for (int i=1; i< argc; ++i) {
+		os << argv[i] << ' ';
+	}
+	string cmdline=os.str();
 
-    Cli::Section::init(cout, cerr, "0.0.1");
-    Cli cli;
-    cli.init(argv[0], "My Project - offline.");
-    return cli.exec(cmdline)?0:1;
+	Cli::Section::init(cout, cerr, "0.0.1");
+	Cli cli;
+	cli.init(argv[0], "My Project - offline.");
+	return cli.exec(cmdline)?0:1;
 }
 
 int main_online(int argc, char** argv) {
 	using namespace std;
 	using Cli = my_namespace::MyHmiOnline;
 
-    ostringstream os;
-    for (int i=1; i< argc; ++i) {
-        os << argv[i] << ' ';
-    }
-    string cmdline=os.str();
+	ostringstream os;
+	for (int i=1; i< argc; ++i) {
+		os << argv[i] << ' ';
+	}
+	string cmdline=os.str();
 
-    Cli::Section::init(cout, cerr, "0.0.1");
-    Cli cli;
-    cli.init(argv[0], "My Project - online.");
-    return cli.exec(cmdline)?0:1;
+	Cli::Section::init(cout, cerr, "0.0.1");
+	Cli cli;
+	cli.init(argv[0], "My Project - online.");
+	return cli.exec(cmdline)?0:1;
 }
 
 int main(int argc, char** argv) {
